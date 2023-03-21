3188cf281d2168a9e13f0ea3f2f12574a89a76e4
dg1223 <alavi1223@hotmail.com>
2023-03-21 18:18:59 -0400

    jsx dynamic data recap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const products = [
+    { name: "laptop", price: 53000 },
+    { name: "phone", price: 53000 },
+    { name: "watch", price: 3300 },
+    { name: "tablet", price: 3500 },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.map((product) => (
+        <Product name={product.name} price={product.price}></Product>
+      ))}
+      {/* <Product name="laptop" price="47000"></Product>
+      <Product name="phone" price="75000"></Product>
+      <Product name="watch" price="3500"></Product> */}
+    </div>
+  );
+}
+
+function Product(props) {
+  return (
+    <div className="product">
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price}</p>
     </div>
   );
 }
